bdffa1a97aa27542ca0a61465f32798c4e796d9c
Webie <90978643+vimal-oneway@users.noreply.github.com>
2023-03-24 21:23:07 +0530

    cors bug

--- src/index.ts
+++ src/index.ts
@@ -91,13 +91,13 @@ app.get(
     res: Response<string, Locals>,
     next: NextFunction
   ) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //   "Access-Control-Allow-Methods",
+    //   "GET, POST, PUT, DELETE, OPTIONS"
+    // );
+    // res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    // res.header("Access-Control-Allow-Credentials", "true");
     res.send("hi");
   }
 );
